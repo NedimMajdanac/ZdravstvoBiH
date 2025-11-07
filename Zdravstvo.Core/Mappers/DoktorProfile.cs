4aa53b1ba4fe23da8a5aa0c4656fa3ceb3ba164f
NedimMajdanac <nedimmajdanac13@gmail.com>
2025-11-07 16:10:28 +0100

    Added AutoMapper for DoktorProfile

---
+++ Zdravstvo.Core/Mappers/DoktorProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zdravstvo.Core.DTOs;
+using Zdravstvo.Core.Entities;
+
+namespace Zdravstvo.Core.Mappers
+{
+    public class DoktorProfile:Profile
+    {
+        public DoktorProfile()
+        {
+            CreateMap<Doktor, DoktorDTO.ReadDoktorDTO>();
+            CreateMap<DoktorDTO.UpdateDoktorDTO, Doktor>();
+            CreateMap<DoktorDTO.CreateDoktorDTO, Doktor>();
+        }
+    }
+}
