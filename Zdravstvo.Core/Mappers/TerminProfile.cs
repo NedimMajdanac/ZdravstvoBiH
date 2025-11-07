ec9bfa0f179855ec0b712ed0c6b42b9a2026e1c0
NedimMajdanac <nedimmajdanac13@gmail.com>
2025-11-07 16:13:42 +0100

    Added AutoMapper for UstanovaProfile and TerminProfile

---
+++ Zdravstvo.Core/Mappers/TerminProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zdravstvo.Core.DTOs;
+using Zdravstvo.Core.Entities;
+
+namespace Zdravstvo.Core.Mappers
+{
+    public class TerminProfile : Profile
+    {
+        public TerminProfile() 
+        {
+            CreateMap<Termin, TerminDTO.ReadTerminDTO>();
+            CreateMap<TerminDTO.CreateTerminDTO, Termin>();
+            CreateMap<TerminDTO.UpdateTerminDTO, Termin>();
+        }
+    }
+}
---
+++ Zdravstvo.Core/Mappers/UstanovaProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zdravstvo.Core.DTOs;
+using Zdravstvo.Core.Entities;
+
+namespace Zdravstvo.Core.Mappers
+{
+    public class UstanovaProfile:Profile
+    {
+        public UstanovaProfile() 
+        {
+            CreateMap<Ustanova, UstanovaDTO.ReadUstanovaDTO>();
+            CreateMap<UstanovaDTO.CreateUstanovaDTO, Ustanova>();
+            CreateMap<UstanovaDTO.UpdateUstanovaDTO, Ustanova>();
+        }
+    }
+}
